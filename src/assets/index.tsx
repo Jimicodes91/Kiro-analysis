5423622759c20269e6dc5175b265984e325278fd
Thermiee <navyblack16@yahoo.com>
2025-03-24 23:15:53 +0100

    add svgs

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,5 +1,23 @@
 import Logo from "./svg/logo.svg";
 import Group from "./svg/Group.svg";
 import OnBoardingGroup from "./svg/onBoardingGroup.svg";
+import Project from "./svg/Project.svg";
+import Message from "./svg/Message.svg";
+import Admin from "./svg/Admin.svg";
+import Client from "./svg/Client.svg";
+import Event from "./svg/Event.svg";
+import House from "./svg/House.svg";
+import Finance from "./svg/Finance.svg";
 
-export { Logo, Group, OnBoardingGroup };
+export {
+  Logo,
+  Group,
+  OnBoardingGroup,
+  Project,
+  Message,
+  Admin,
+  Client,
+  Event,
+  House,
+  Finance,
+};
