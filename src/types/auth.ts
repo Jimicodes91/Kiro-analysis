3aeced08b532db47840f6b24e77bf9484603784c
Thermiee <navyblack16@yahoo.com>
2025-03-24 19:19:12 +0100

    fix error response

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,52 +1,52 @@
 export interface AdminSignUpProps {
-email: string
-password: string
+  email: string;
+  password: string;
 }
 export interface AdminSignUpFormProps extends AdminSignUpProps {
-  confirmPassword: string
+  confirmPassword: string;
 }
 export interface CompanyAdminSignUpProps extends AdminSignUpProps {
-name: string
+  name: string;
 }
 
 export interface LoginUser {
-email: string
-password: string
+  email: string;
+  password: string;
 }
 export interface CompleteRegProps {
-email: string
-password: string
-companyId: string
+  email: string;
+  password: string;
+  companyId: string;
 }
 export interface SendConsultantInviteProps {
-email: string
-role: string
+  email: string;
+  role: string;
 }
 export interface UpdatePasswordProps {
-newPassword: string
-currentPassword: string
+  newPassword: string;
+  currentPassword: string;
 }
 export interface ResetPasswordProps {
-token: string
-newPassword: string
+  token: string;
+  newPassword: string;
 }
 export interface TokenProp {
-token: string
+  token: string;
 }
 export interface EmailProp {
-email: string
+  email: string;
 }
 export interface AddClientProp {
-name: string
-email: string
+  name: string;
+  email: string;
 }
 
 export interface CompanyProps {
-    name: string;
-    industryType: string;
-    size: string;
-    country: string;
-    address: string;
-    city: string;
-    postalCode: string;
-  }
+  name: string;
+  industryType: string;
+  size: string;
+  country: string;
+  address: string;
+  city: string;
+  postalCode: string;
+}
