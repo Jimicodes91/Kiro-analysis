05b731203254c2af9ec780025c4723481e249396
Johnbosco Okolo <john.bosco@mukuru.com>
2025-04-08 21:47:01 +0100

    feat: installed shadsn components

--- src/Components/Toast/index.tsx
+++ src/Components/Toast/index.tsx
@@ -1,78 +1,89 @@
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { ErrorIcon, InfoIcon, SuccessIcon, WarningIcon } from "../../assets/icons/index";
 import "../../index.css";
-import ToastTemplate from './ToastTemplate';
-import { ErrorIcon, InfoIcon, SuccessIcon, WarningIcon } from '../../assets/icons/index';
-
+import ToastTemplate from "./ToastTemplate";
 
 const Toast = {
   success: (message: string) => {
-    toast(<ToastTemplate message={message} icon={<div className="toast-icon"><SuccessIcon /></div>} />, {
-      position: "top-right",
-      autoClose: 2000,
-      toastId: "customId",
-      hideProgressBar: true,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      pauseOnFocusLoss: true,
-      style: {
-        background: "#daf1e3",
-        boxShadow:'none',
-        borderRadius:'8px'
-      },
-    });
+    toast(
+      <ToastTemplate
+        message={message}
+        icon={
+          <div className="toast-icon">
+            <SuccessIcon />
+          </div>
+        }
+      />,
+      {
+        toastId: "customId",
+        style: {
+          background: "#daf1e3",
+          boxShadow: "none",
+          borderRadius: "8px",
+        },
+      }
+    );
   },
   error: (message: string) => {
-    toast(<ToastTemplate message={message} icon={<div className="toast-icon" style={{ fontSize: "32px" }}><ErrorIcon /></div>} />, {
-      position: "top-right",
-      autoClose: 2000,
-      toastId: "customId",
-      hideProgressBar: true,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      pauseOnFocusLoss: true,
-      style: {
-        background: "#F6DFDF",
-        boxShadow:'none',
-        borderRadius:'8px'
-      },
-    });
+    toast(
+      <ToastTemplate
+        message={message}
+        icon={
+          <div className="toast-icon" style={{ fontSize: "32px" }}>
+            <ErrorIcon />
+          </div>
+        }
+      />,
+      {
+        toastId: "customId",
+        style: {
+          background: "#F6DFDF",
+          boxShadow: "none",
+          borderRadius: "8px",
+        },
+      }
+    );
   },
   warning: (message: string) => {
-    toast(<ToastTemplate message={message} icon={<div className="toast-icon" style={{ fontSize: "32px" }}><WarningIcon /></div>} />, {
-      position: "top-right",
-      toastId: "customId",
-      autoClose: 2000,
-      hideProgressBar: true,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      pauseOnFocusLoss: true,
-      style: {
-        background: "#F7EACA",
-        boxShadow:'none',
-        borderRadius:'8px'
-      },
-    });
+    toast(
+      <ToastTemplate
+        message={message}
+        icon={
+          <div className="toast-icon" style={{ fontSize: "32px" }}>
+            <WarningIcon />
+          </div>
+        }
+      />,
+      {
+        toastId: "customId",
+        style: {
+          background: "#F7EACA",
+          boxShadow: "none",
+          borderRadius: "8px",
+        },
+      }
+    );
   },
   info: (message: string) => {
-    toast(<ToastTemplate message={message} icon={<div className="toast-icon" style={{ fontSize: "32px" }}><InfoIcon /></div>} />, {
-      position: "top-right",
-      autoClose: 4000,
-      toastId: "customId",
-      hideProgressBar: true,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      pauseOnFocusLoss: true,
-      style: {
-        background: "#D5E2FF",
-        boxShadow:'none',
-        borderRadius:'8px'
-      },
-    });
+    toast(
+      <ToastTemplate
+        message={message}
+        icon={
+          <div className="toast-icon" style={{ fontSize: "32px" }}>
+            <InfoIcon />
+          </div>
+        }
+      />,
+      {
+        toastId: "customId",
+        style: {
+          background: "#D5E2FF",
+          boxShadow: "none",
+          borderRadius: "8px",
+        },
+      }
+    );
   },
 };
 
