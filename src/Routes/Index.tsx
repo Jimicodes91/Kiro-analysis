023f96ddef13dc321362226c97b1f5229d2b7340
Okolo Johnbosco <chukwuemekaokolo233@gmail.com>
2025-04-10 16:23:07 +0100

    Delete src/Routes directory
    Renamed to routes

--- src/Routes/Index.tsx
+++
@@ -1,6 +0,0 @@
-import { useRoutes } from "react-router-dom";
-import { AuthRoutes, HomeRoutes } from "./AppRoutes";
-
-const Routes = () => useRoutes([...AuthRoutes, HomeRoutes]);
-
-export default Routes;
