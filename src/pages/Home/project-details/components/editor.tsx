e9d7ebbba8be54d660588041bb62d5ed3b061ac0
Johnbosco Okolo <chukwuemekaokolo233@gmail.com>
2025-05-02 10:50:43 +0100

    fix: create note cleanup

--- src/pages/Home/project-details/components/editor.tsx
+++ src/pages/Home/project-details/components/editor.tsx
@@ -70,7 +70,7 @@ export const NoteEditor = ({ projectId }: { projectId: string }) => {
               htmlFor="terms"
               className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
             >
-              Mark Visible to client
+              Make visible to client
             </label>
           </div>
 
