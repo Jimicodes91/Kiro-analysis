8a9c6d91ea6c1307285b329867fdb7ffba87a446
KOLAWOLE SALAMI <SALAMIKOLAWOLE3@GMAIL.COM>
2026-03-21 00:06:48 -0700

    refactor: remove inline milestone edit/delete/add from table - handled by edit journey modal

--- src/pages/Home/Admin/journey/milestone-table-row.tsx
+++ src/pages/Home/Admin/journey/milestone-table-row.tsx
@@ -1,86 +1,21 @@
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Icons } from "@/components/ui/icons";
 import { TableCell, TableRow } from "@/components/ui/table";
 import useGetAllProjectTypeMilestones, {
-  ProjectTypeMilestone,
+    ProjectTypeMilestone,
 } from "@/hooks/project-modules/milestones/use-all-get-project-type-milestones";
-import useDisclosure from "@/hooks/use-disclosure";
-import { AnimatePresence } from "framer-motion";
-import DeleteMilestoneModal from "./delete-milestone-modal";
-import MilestoneForm from "./milestone-form";
 
 function MilestoneTableRow({
   milestone,
-  refetch,
-  projectTypeId,
 }: {
   milestone: ProjectTypeMilestone;
   refetch: ReturnType<typeof useGetAllProjectTypeMilestones>["refetch"];
   projectTypeId: string;
 }) {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const {
-    isOpen: isDeleteMilestoneOpen,
-    onOpen: onDeleteMilestoneOpen,
-    onClose: onDeleteMilestoneClose,
-  } = useDisclosure();
-
   return (
-    <>
-      {isOpen ? (
-        <MilestoneForm
-          onClose={onClose}
-          milestone={milestone}
-          refetch={refetch}
-          key={milestone.updated_at}
-          projectTypeId={projectTypeId}
-        />
-      ) : (
-        <TableRow className="">
-          <TableCell>{milestone?.name}</TableCell>
-          <TableCell>{milestone?.duration}</TableCell>
-          <TableCell>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="ghost" size="icon">
-                  <Icons.more className="size-5" />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="w-40" align="end" forceMount>
-                <DropdownMenuGroup>
-                  <>
-                    <DropdownMenuItem onClick={onOpen}>Edit Milestone</DropdownMenuItem>
-                    <DropdownMenuItem onClick={onDeleteMilestoneOpen}>
-                      Delete Milestone
-                    </DropdownMenuItem>
-                  </>
-                </DropdownMenuGroup>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </TableCell>
-        </TableRow>
-      )}
-      <AnimatePresence initial={false} mode="wait" onExitComplete={() => null}>
-        {isDeleteMilestoneOpen && (
-          <DeleteMilestoneModal
-            isOpen={isDeleteMilestoneOpen}
-            onClose={onDeleteMilestoneClose}
-            milestone={milestone}
-            projectTypeId={projectTypeId}
-            onSuccess={() => {
-              refetch();
-            }}
-          />
-        )}
-      </AnimatePresence>
-    </>
+    <TableRow>
+      <TableCell>{milestone?.name}</TableCell>
+      <TableCell>{milestone?.duration}</TableCell>
+      <TableCell />
+    </TableRow>
   );
 }
 export default MilestoneTableRow;
--- src/pages/Home/Admin/journey/milestone-table.tsx
+++ src/pages/Home/Admin/journey/milestone-table.tsx
@@ -1,21 +1,14 @@
-import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
+    Table,
+    TableBody,
+    TableHead,
+    TableHeader,
 } from "@/components/ui/table";
 import TableSkeletonRowLoader, { EmptyTable } from "@/components/ui/table-row-skeleton";
 import useGetAllProjectTypeMilestones from "@/hooks/project-modules/milestones/use-all-get-project-type-milestones";
 import { ProjectType } from "@/hooks/project-modules/project-types/use-get-all-project-types";
-import useDisclosure from "@/hooks/use-disclosure";
 import { cn } from "@/lib/utils";
 import { motion } from "framer-motion";
-import { LuPlus } from "react-icons/lu";
-import MilestoneForm from "./milestone-form";
 import MilestoneTableRow from "./milestone-table-row";
 
 function MilestoneTable({
@@ -28,7 +21,6 @@ function MilestoneTable({
   isOpen: boolean;
 }) {
   const getMilestones = useGetAllProjectTypeMilestones(projectType.id);
-  const { isOpen: isCreateFormOpen, onOpen, onClose } = useDisclosure();
 
   const renderTableBody = () => {
     if (getMilestones.isPending)
@@ -87,36 +79,6 @@ function MilestoneTable({
               <TableHead className="w-4"></TableHead>
             </TableHeader>
             <>{renderTableBody()}</>
-
-            <TableBody>
-              {isCreateFormOpen && (
-                <MilestoneForm
-                  onClose={onClose}
-                  refetch={getMilestones.refetch}
-                  key={String(isOpen)}
-                  projectTypeId={projectType.id}
-                />
-              )}
-              <TableRow className="!bg-white !border-t-0">
-                <TableCell colSpan={3} className="w-fit">
-                  <div className="grid">
-                    <div className="items-center gap-3 flex">
-                      <Button
-                        leftIcon={<LuPlus />}
-                        variant="ghost"
-                        onClick={onOpen}
-                        size="sm"
-                        className="px-0 hover:bg-transparent"
-                      >
-                        Add stage
-                      </Button>
-
-                      <Separator className="w-fit" />
-                    </div>
-                  </div>
-                </TableCell>
-              </TableRow>
-            </TableBody>
           </Table>
         </motion.td>
       </motion.tr>
